0a3a00b61e8b3061b42649cc25482facc2d6ba64
MariaAdel-22 <maria.espamoya@gmail.com>
2022-05-24 19:15:33 +0200

    Add project files.

---
+++ ProyectoApiAWS/Controllers/CochesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoApiAWS.Models;
+using ProyectoApiAWS.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoApiAWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CochesController : ControllerBase
+    {
+        private RepositoryCoches repoCoches;
+
+        public CochesController(RepositoryCoches repoCoches) {
+
+            this.repoCoches = repoCoches;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Coche>> GetCoches() {
+
+            return this.repoCoches.GetCoches();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Coche> FindCoche(string id) {
+
+            return this.repoCoches.BuscarCoche(id);
+        }
+
+        [HttpPost]
+        public ActionResult InsertCoche(Coche coche) {
+
+            this.repoCoches.InsertarCoche(coche.Marca, coche.Modelo, coche.Imagen);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult UpdateCoche(Coche coche) {
+
+            this.repoCoches.ModificarCoche(coche.Marca, coche.Modelo, coche.Imagen);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCoche(string id) {
+
+            this.repoCoches.EliminarCoche(id);
+
+            return Ok();
+        }
+    }
+}
---
+++ ProyectoApiAWS/Repositories/RepositoryCoches.cs
@@ -0,0 +1,68 @@
+﻿using ProyectoApiAWS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoApiAWS.Repositories
+{
+    public class RepositoryCoches
+    {
+        public List<Coche> coche;
+        public RepositoryCoches() {
+
+            List<Coche> coches = new List<Coche>
+            {
+                new Coche{ Marca="Hyundai",Modelo="1.2 MPI KLASS",Imagen="https://d1eip2zddc2vdv.cloudfront.net/dphotos/750x400/12904948-1602182075.jpeg"},
+                new Coche{ Marca="Mercedes",Modelo="Clase A",Imagen="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/2018_Mercedes-Benz_A200_AMG_Line_Premium%2B_1.3_Front.jpg/250px-2018_Mercedes-Benz_A200_AMG_Line_Premium%2B_1.3_Front.jpg"}
+            };
+
+            this.coche = coches;
+        }
+
+        public List<Coche> GetCoches() {
+
+            return this.coche;
+        }
+
+        public Coche BuscarCoche(string Marca) {
+
+            Coche nuevoCoche= new Coche();
+
+            foreach (Coche coche in this.coche) {
+
+                if (coche.Marca == Marca)
+                {
+
+                    nuevoCoche = coche;
+                }
+            }
+
+            return nuevoCoche;
+        }
+
+        public void InsertarCoche(string Marca,string Modelo,string Imagen) {
+
+            Coche coche = new Coche { Marca = Marca, Modelo = Modelo, Imagen = Imagen };
+
+            this.coche.Add(coche);
+        }
+
+        public void ModificarCoche(string Marca, string Modelo, string Imagen)
+        {
+
+            Coche coche = BuscarCoche(Marca);
+
+            coche.Modelo = Modelo;
+            coche.Imagen = Imagen;
+
+        }
+
+        public void EliminarCoche(string Marca) {
+
+            Coche coche = BuscarCoche(Marca);
+
+            this.coche.Remove(coche);
+        }
+    }
+}
